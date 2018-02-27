ea1e30ecab7fbeef5b3ec2eca084a9f0d5162d20
illusionized10 <colincohan10@gmail.com>
2018-02-27 10:57:54 -0800

    Analytics

--- wp-content/themes/calin-cohan/header.php
+++ wp-content/themes/calin-cohan/header.php
@@ -17,6 +17,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PNSRNHN');</script>
+    <!-- End Google Tag Manager -->
+
     <!-- GOOGLE FONTS -->
     <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,700,900' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
@@ -49,8 +57,13 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
 
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PNSRNHN"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<div id="page" class="site">
     <!-- PRELOADER -->
     <div id="main-preloader" class="main-preloader semi-dark-background">
         <div class="container full-height">
